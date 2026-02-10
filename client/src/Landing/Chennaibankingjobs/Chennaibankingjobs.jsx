128c3f06dfeae0388bbb63922d8358fb533df1f6
BM-ACADEMY <teamkamarweb@gmail.com>
2026-02-10 16:11:38 +0530

    updated

--- client/src/Landing/Chennaibankingjobs/Chennaibankingjobs.jsx
+++ client/src/Landing/Chennaibankingjobs/Chennaibankingjobs.jsx
@@ -69,27 +69,6 @@ const LandingPage = () => {
           content="Apply for Chennai banking jobs with Core Talents. Immediate hiring for banking customer support roles with flexible shifts and growth opportunities. Apply now."
         />
       </Helmet>
-      <Helmet>
-        <script>
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '25645784465090426');
-            fbq('track', 'PageView');
-          `}
-        </script>
-        <noscript>
-          {`<img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=25645784465090426&ev=PageView&noscript=1"
-          />`}
-        </noscript>
-      </Helmet>
       {/* <Navbar /> */}
       <main className="pt-24 pb-20 space-y-24">
         <HeroSection />
